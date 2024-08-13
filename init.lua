b17831a50747abb66177cde39c69a43dfacc41a0
Skesky <kevin03023@gmail.com>
2024-08-14 00:13:00 +0200

    my neovim configuration

---
+++ init.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+
+require("config.settings")
+require("config.lazy")
+require("keymaps.maps")
---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,9 @@
+-- ~/nvim/lua/plugins/colorizer.lua
+
+return {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+        require("colorizer").setup({ "*" })
+    end,
+}
+
